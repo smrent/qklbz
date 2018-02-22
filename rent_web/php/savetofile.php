c3917ee22f848d165deba4e9109da5d5f9afed91
smrent <32243525+smrent@users.noreply.github.com>
2018-02-22 00:48:10 -0500

    Delete savetofile.php

--- rent_web/php/savetofile.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (isset($_FILES['myFile'])) {
-    // Example:
-    move_uploaded_file($_FILES['myFile']['tmp_name'], "../uploads/" . $_FILES['myFile']['name']);
-    echo 'successful';
-}
-?>
